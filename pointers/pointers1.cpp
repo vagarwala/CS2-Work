5032a49714f93c029922dc7b674aae4837f14ca9
Aaron Feldman <feldmando@gmail.com>
2016-01-05 16:09:11 -0800

    Initial commit
    Move code from private repo

---
+++ pointers/pointers1.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file pointers1.cpp
+ * @author The CS2 TA Team <cs2tas@caltech.edu>
+ * @date 2015
+ * @copyright This code is in the public domain.
+ *
+ * @brief Pointer-fu Exercise 1: Pointer declarations
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Declares and sets values of pointers.
+ */
+int main(int argc, char *argv[])
+{
+    /* TODO: Put your declarations here.
+     * All variables are of type `int`. Only use one line! */
+
+    /***** CHANGE NOTHING BELOW THIS LINE *****/
+    a = 5;
+    b = &a;
+    c = *b;
+    d = &b;
+    /***** CHANGE NOTHING ABOVE THIS LINE *****/
+
+    return 0;
+}
